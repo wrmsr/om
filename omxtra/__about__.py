2987ff91379cd60e0639280e0a640e2b473f4641
wrmsr <timwilloney@gmail.com>
2026-08-13 17:19:13 -0700

    Remove obsolete C‑extension packaging files and disable the cext flag
    The entire .pkg/omxtra‑cext directory, including its build metadata and symlinks, is removed as it is no longer used. The Setuptools configuration in __about__.py is updated by dropping the `cext = True` attribute.

--- omxtra/__about__.py
+++ omxtra/__about__.py
@@ -39,8 +39,6 @@ class Project(ProjectBase):
 
 
 class Setuptools(SetuptoolsBase):
-    cext = True
-
     find_packages = {
         'include': [Project.name, f'{Project.name}.*'],
         'exclude': [*SetuptoolsBase.find_packages['exclude']],
